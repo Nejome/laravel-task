6681b57ea44f921461a718e55fc5de7465012669
Alnejome Mubark <alnejome98@gmail.com>
2023-09-08 15:18:28 +0300

    remove Procfile

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\ApplicationController;
-    
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
